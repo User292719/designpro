ccd40d41af0fc81c2901c97c709656710ee18775
User292719 <nasok4311@gmail.com>
2023-11-15 20:35:01 +0300

    password_validation

--- cabinet/forms.py
+++ cabinet/forms.py
@@ -11,23 +11,29 @@ class ConfirmField(forms.Field):
                 "Поставьте галочку о согласии"
             )
 
-def password_validator(form, field):
-    if form.password.data != field.data:
-        raise forms.ValidationError(
-            "Ошибка: "
-            "введеные пароли не совпадают"
-        )
+# def password_validator(self, data):
+#     print(data)
+#     if self.password.data != data:
+#         raise forms.ValidationError(
+#             "Ошибка: "
+#             "введеные пароли не совпадают"
+#         )
 
 class UserForm(forms.ModelForm):
     username = forms.CharField()
     first_name = forms.CharField()
     email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput(), validators=[password_validator])
+    password2 = forms.CharField(widget=forms.PasswordInput())
     confirm = ConfirmField(required=False)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email','password']
 
-
+    def clean_password2(self):
+        if self.cleaned_data.get("password") != self.cleaned_data.get("password2"):
+            raise forms.ValidationError(
+                "Ошибка: "
+                "введеные пароли не совпадают"
+            )
